dabb2ea4b9813b2893012152860f285230dfea67
József Mandula <63676153+joealmond@users.noreply.github.com>
2021-12-16 21:52:46 +0100

    reaload after deploy

---
+++ dist/js/hook.js
@@ -0,0 +1,11 @@
+let newDeployHash = Math.round(Math.random() * 10000);
+sessionStorage.setItem("deployHash", newDeployHash);
+let oldDeployHash = sessionStorage.getItem("deployHash");
+
+function isDeployHashEqual() {
+  if (oldDeployHash === newDeployHash) {
+  } else if (oldDeployHash !== newDeployHash) {
+    location.reload();
+  }
+}
+setInterval(isDeployHashEqual, 5000);
